f5c838b846159052d0d187863bb7e5451f901e87
solik003 <malugasola@gmail.com>
2025-04-16 11:39:52 +0300

    fix mongodb connection

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,12 +1,39 @@
-import mongoose from "mongoose";
 
-const connectMongoDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL as string);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log(error);
+import mongoose from 'mongoose';
+
+const MONGO_URL = process.env.MONGO_URL || '';
+
+if (!MONGO_URL) {
+    throw new Error('Please define the MONGO_URL environment variable');
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectMongoDB() {
+    if (cached.conn) {
+        return cached.conn;
     }
-};
 
-export default connectMongoDB;
+    if (!cached.promise) {
+        cached.promise = mongoose
+            .connect(MONGO_URL, {
+                dbName: 'memesDB',
+                bufferCommands: false,
+            })
+            .then((mongoose) => {
+                console.log('Connected to MongoDB');
+                return mongoose;
+            })
+            .catch((err) => {
+                console.error('MongoDB connection error:', err);
+                throw err;
+            });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
