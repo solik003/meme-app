5094c13e3eaddccddd5f02ab363bf568414b2c07
solik003 <malugasola@gmail.com>
2025-04-16 13:09:59 +0300

    remove router on pages

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,25 +2,11 @@
 'use client';
 
 import MemeList from '@/components/MemeList';
-import { useRouter } from 'next/navigation';
 
 
 export default function ListPage() {
-    const router = useRouter();
-
-    const handleBack = () => {
-        router.back();
-    };
-
     return (
         <div className="p-4">
-            <button
-                onClick={handleBack}
-                className="text-sm text-blue-600 hover:underline mb-4"
-            >
-                &larr; Back
-            </button>
-
             <MemeList />
         </div>
     );
--- app/table/page.tsx
+++ app/table/page.tsx
@@ -2,24 +2,10 @@
 
 import { JSX } from 'react';
 import MemeTable from '@/components/MemeTable';
-import { useRouter } from 'next/navigation';
 
 export default function TablePage(): JSX.Element {
-    const router = useRouter();
-
-    const handleBack = () => {
-        router.back();
-    };
-
     return (
         <div className="p-5">
-            <button
-                onClick={handleBack}
-                className="text-sm text-blue-600 hover:underline mb-4"
-            >
-                &larr; Back
-            </button>
-            <h2 className="text-2xl font-semibold mb-4">Meme Table</h2>
             <MemeTable />
         </div>
     );
