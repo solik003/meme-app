00b4d581b5cf0cb5d4c511dbe7518577a3a502a8
solik003 <malugasola@gmail.com>
2025-04-16 11:42:21 +0300

    add validation on backend

--- models/Meme.ts
+++ models/Meme.ts
@@ -1,6 +1,7 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema, Types, models } from 'mongoose';
 
-export interface IMeme extends Document {
+export interface IMeme extends Document {  
+    _id: string;
     name: string;
     image: string;
     likes: number;
@@ -9,11 +10,42 @@ export interface IMeme extends Document {
 
 const MemeSchema: Schema = new Schema(
     {
-        name: { type: String, required: true },
-        image: { type: String, required: true },
-        likes: { type: Number, default: 0 },
-        link: { type: String, required: true },
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            minlength: [3, 'Name must be at least 3 characters'],
+            maxlength: [100, 'Name must be at most 100 characters'],
+        },
+        image: {
+            type: String,
+            required: [true, 'Image is required'],
+            validate: {
+                validator: function (value: string) {
+                    return /^https?:\/\/.+\.(jpg|jpeg)$/i.test(value);
+                },
+                message: 'Image must be a valid JPG URL',
+            },
+        },
+        likes: {
+            type: Number,
+            required: true,
+            min: [0, 'Likes must be at least 0'],
+            max: [99, 'Likes cannot exceed 99'],
+            default: 0,
+        },
+        link: {
+            type: String,
+            required: [true, 'Link is required'],
+            validate: {
+                validator: function (value: string) {
+                    return /^https?:\/\/.+/i.test(value);
+                },
+                message: 'Link must be a valid URL',
+            },
+        },
     }
 );
 
-export default mongoose.model<IMeme>('Meme', MemeSchema);
+const Meme = models.Meme || mongoose.model<IMeme>('Meme', MemeSchema);
+
+export default Meme;
