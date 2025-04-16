06610bd94834bc7743a5a57e22fa524076d4d654
solik003 <malugasola@gmail.com>
2025-04-16 10:39:05 +0300

    fix List

--- components/MemeList.tsx
+++ components/MemeList.tsx
@@ -12,14 +12,16 @@ export default function MemeList() {
     if (error) return <p className="text-red-500">Error: {(error as Error).message}</p>;
 
     return (
-        <div className="grid grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-            {memes?.map((meme) => (
-                <MemeCard
-                    key={meme.id}
-                    meme={meme}
-                    onClick={() => setSelectedMeme(meme.id)}
-                />
-            ))}
+        <div className="flex justify-center">
+            <div className="grid grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                {memes?.map((meme) => (
+                    <MemeCard
+                        key={meme.id}
+                        meme={meme}
+                        onClick={() => setSelectedMeme(meme.id)}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
